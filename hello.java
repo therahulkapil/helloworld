3921f998cafbc8884b18da017cdb069a2b2e2411
therahulkapil <therahulkapil@gmail.com>
2022-11-19 15:55:15 +0530

    updated helloworld

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
 			System.out.println("Hello world java..."+i);
+			System.out.println("go next...");
 		}
 	}
 }
